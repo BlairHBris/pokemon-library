9dc34f3b5e35dfa12b4fca3e94092908dc88a868
Blair Bristow <blairhbristow@gmail.com>
2022-03-30 18:25:16 -0400

    Begin alt method

--- site/index.js
+++ site/index.js
@@ -1,19 +1,48 @@
+const api = "https://pokeapi.co/api/v2/pokemon?limit=50&offset=0"
 const main = document.querySelector("main")
 const ul = document.querySelector("ul")
 
-for (var i = 0; i < 50; i++) {
+fetch(api)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        let pokemons = data;
+
+        pokemons.map(function (pokemon) {
+            let li = document.createElement('li')
+            let figure = document.createElement('figure')
+            let img = document.createElement('img')
+            let figcaption = document.createElement('figcaption')
+
+            figcaption.innerHTML = `${pokemon.name}`
+            img.src = pokemon.url
+            img.alt = `${pokemon.name}`
+
+            figure.append(img)
+            figure.append(figcaption)
+            li.append(figure)
+            ul.append(li)
+        })
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+
+/*for (var i = 0; i < 50; i++) {
     const li = document.createElement("li")
     li.classList.add("pokemon-listing")
     const figure = document.createElement("figure")
     const img = document.createElement("img")
     const figcaption = document.createElement("figcaption")
     figcaption.innerHTML = "Pokemon Name Goes Here"
-    img.src = "pokemon-image-url-goes-here.jpg" 
-    img.alt="Pokemon Name Goes Here" 
+    img.src = "pokemon-image-url-goes-here.jpg"
+    img.alt = "Pokemon Name Goes Here"
     figure.append(img)
     figure.append(figcaption)
     li.append(figure)
     ul.append(li)
 }
 
-main.append(divContainer)
+*/
+
