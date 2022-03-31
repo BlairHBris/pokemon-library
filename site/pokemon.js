4dcb43772b96bfcd841d12d346e56c81e905b0cb
Blair Bristow <blairhbristow@gmail.com>
2022-03-31 17:28:50 -0400

    Add abilities

--- site/pokemon.js
+++ site/pokemon.js
@@ -1,6 +1,7 @@
 const pokemon = document.querySelector("#pokemon")
 const spinner = document.querySelector(".spinner")
 const pokemonDetails = document.querySelector("#pokemon-details")
+const ul = document.querySelector("ul")
 
 function addPokemon(pokemon) {
     const div = document.createElement("div")
@@ -13,6 +14,18 @@ function addPokemon(pokemon) {
     pokemonDetails.append(div)
 }
 
+function addAbilities(pokemon) {
+    const li = document.createElement("li")
+
+    li.innerHTML = `
+    <span class="ability-name">${pokemon.name}</span>
+    <br>
+    <span class="ability-short-description">${pokemon.effect_entries[1].short_effect}</span>
+    `
+    ul.append(li)
+
+}
+
 const url = new URL(window.location)
 const queryString = new URLSearchParams(url.search)
 
@@ -21,5 +34,21 @@ fetch(`https://pokeapi.co/api/v2/pokemon/${queryString.get("pokemon")}`)
         return response.json()
     }).then(parsedResponse => {
         addPokemon(parsedResponse)
+        const abilitiesRequests = parsedResponse.abilities
+            .map(parsedResponse => parsedResponse.ability.url)
+            .map(url => {
+                return fetch(url).then(response => response.json())
+            })
+        return Promise.all(abilitiesRequests)
+    }).then(parsedResponses =>{
+        console.log(parsedResponses)
         spinner.classList.add("hidden")
+        parsedResponses.forEach(parsedResponse => {
+            addAbilities(parsedResponse)
     })
+})
+
+
+
+
+
