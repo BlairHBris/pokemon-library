ad1339f255ec5ff2ee342511d9cfe641b406f707
Blair Bristow <blairhbristow@gmail.com>
2022-03-31 13:50:24 -0400

    Add individual pokemon page and get picture and name of pokemon onto page

--- site/pokemon.js
+++ site/pokemon.js
@@ -1,16 +1,28 @@
 const pokemon = document.querySelector("#pokemon")
 const spinner = document.querySelector(".spinner")
+const ul = document.querySelector("ul")
 
 function addPokemon(pokemon) {
+    console.log(pokemon)
     const li = document.createElement('li')
     const figure = document.createElement('figure')
 
     figure.innerHTML = `
-    <a href="pokemon.html?pokemon=${pokemon.name}">
-        <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}"/>
-    </a>
+    <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}"/>
+    <figcaption><a href="pokemon.html?pokemon=${pokemon.name}">${pokemon.name}</a></figcaption>
     `
 
     li.append(figure)
     ul.append(li)
 }
+
+const url = new URL(window.location)
+const queryString = new URLSearchParams(url.search)
+
+fetch(`https://pokeapi.co/api/v2/pokemon/${queryString.get("pokemon")}`)
+    .then(response => {
+        return response.json()
+    }).then(parsedResponse => {
+        addPokemon(parsedResponse)
+        spinner.classList.add("hidden")
+    })
